4e30bc70a0e64b484471ca10174ef6defe1f9c78
Dave <code-public@netbeing.org>
2025-08-20 13:04:33 -0700

    Add module info

---
+++ java/lib/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module cheap {
+    exports net.netbeing.cheap.model;
+    exports net.netbeing.cheap.impl.basic;
+    exports net.netbeing.cheap.impl.reflect;
+    exports net.netbeing.cheap.util;
+}
